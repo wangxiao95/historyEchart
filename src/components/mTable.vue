557295e82f56603d7844c647e15ba0679896335b
wangxiao <15133676286@163.com>
2019-05-13 17:35:24 +0800

    修改配置

---
+++ src/components/mTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <table>
+    <thead>
+      <th></th>
+    </thead>
+  </table>
+</template>
+
+<script>
+  export default {
+    name: "table"
+  }
+</script>
+
+<style scoped>
+
+</style>
